03a592eb649b27a51aad81a78234a46806d001ec
s18606 <61777941+s18606@users.noreply.github.com>
2020-03-23 04:09:36 +0000

    s18606

---
+++ New Unity Project/Assets/asd.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class asd : MonoBehaviour
+{
+
+  
+    
+void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Ziemniaczek")
+        {
+            StartCoroutine(TimeToWait());
+        }
+    }
+    IEnumerator TimeToWait()
+    {
+        yield return new WaitForSeconds(.4f);
+        Destroy(gameObject);
+
+    }
+}
+
---
+++ New Unity Project/Assets/script.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class script : MonoBehaviour
+{
+public float speed = 500;
+public float jump = 500;
+
+private Rigidbody2D rb;
+private bool isGrounded = true;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+		
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xDisplacement = Input.GetAxis("Horizontal")* speed* Time.deltaTime;
+		rb.velocity = new Vector2(xDisplacement, rb.velocity.y); 
+		
+		  if (Input.GetKeyDown("space") && (isGrounded ==true)) {
+            rb.AddForce(new Vector2(0, jump));
+            isGrounded = false;
+            
+		}
+		
+    }
+	
+	  void OnCollisionEnter2D(Collision2D col)
+    {
+        isGrounded = true;
+    }
+	
+}
